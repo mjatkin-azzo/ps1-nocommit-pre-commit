58a60d5da57dfca6bbbae7547c874bcebaeb2c08
Cary Symes <SESA628551@se.com>
2022-09-29 12:38:42 +1000

    Improve configuration and configurability

--- nocommit.ps1
+++ nocommit.ps1
@@ -1,11 +1,17 @@
-#!powershell.exe
+#!powershell.exe -File
 
-$searchstr = 'nocommit';
-$diff = git diff --staged -i --diff-filter=d --name-only -G "$searchstr" --relative $(pwd);
-$numfiles = ($diff | Measure-Object -Line).Lines;
+# [CmdletBinding(PositionalBinding = $false)]
+param(
+    [string] $searchstr = 'nocommit',
+    [Parameter(Position=0, ValueFromRemainingArguments = $true)][string[]] $remaining_files
+)
 
-if ($numfiles -gt 0) {
-    Write-Host "Error: string $searchstr was found in $numfiles file(s):" -ForegroundColor Red;
-    Write-Host "$($diff | Out-String)" -ForegroundColor Red -NoNewline;
+[string[]] $diff_files = git diff --staged -i --diff-filter=d --name-only -G "$searchstr" $remaining_files;
+
+if ($diff_files.count -gt 0) {
+    Write-Host "Error: commit gate phrase '$searchstr' was found in $($diff_files.count) file(s):" -ForegroundColor Red;
+    Write-Host "$($diff_files | Out-String)" -ForegroundColor Red -NoNewline;
     exit 1;
 }
+
+exit 0;
